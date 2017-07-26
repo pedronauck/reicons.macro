aa88c7f59da9a74913aaf3907ac547f4de4ff881
Pedro Nauck <pedronauck@gmail.com>
2017-07-26 12:07:03 -0300

    feat: add tests

---
+++ __tests__/macro.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const pluginTester = require('babel-plugin-tester')
+const plugin = require('babel-macros')
+
+const projectRoot = path.join(__dirname, '../')
+
+expect.addSnapshotSerializer({
+  print(val) {
+    return val.split(projectRoot).join('<PROJECT_ROOT>/')
+  },
+  test(val) {
+    return typeof val === 'string'
+  }
+})
+
+pluginTester({
+  plugin,
+  snapshot: true,
+  tests: withFilename([{
+    title: 'as function',
+    code: `
+      const reicons = require('../reicons.macro')
+      const IcCheck = reicons('./tick.svg')
+    `
+  }])
+})
+
+function withFilename(tests) {
+  return tests.map(t => {
+    const test = { babelOptions: { filename: __filename } }
+
+    if (typeof t === 'string') {
+      test.code = t
+    }
+    else {
+      Object.assign(test, t)
+      test.babelOptions.parserOpts = test.babelOptions.parserOpts || {}
+    }
+
+    Object.assign(test.babelOptions.parserOpts, {
+      plugins: ['jsx']
+    })
+
+    return test
+  })
+}
